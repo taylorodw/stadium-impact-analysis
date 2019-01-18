683a0917f9fe796bbe9b3fd48eb4895c95f46440
taylorodw <45613904+taylorodw@users.noreply.github.com>
2019-01-18 12:06:35 -0800

    Update ny_request.r
    add 2017

--- ny_request.r
+++ ny_request.r
@@ -130,3 +130,25 @@ ny_2016 <- get_acs(geography = "county",
                         output = "wide")
 
 write.csv(ny_2016, file = "D:\\Stadiums Project\\NY data\\raw\\ny_2016.csv")
+
+ny_2017 <- get_acs(geography = "county",
+                        variables =
+                            c(hhincome = "B19013_001",
+                              medage = "B01002_001",
+                              poppoverty = "B05010_002",
+                              popfoodstamps = "B22001_002",
+                              popunemployed = "B23025_005",
+                              totalpop = "B01003_001",
+                              popwhite = "B02001_002",
+                              popblack = "B02001_003",
+                              popakna = "B02001_004",
+                              popasian = "B02001_005",
+                              pophipi = "B02001_006",
+                              popother = "B02001_007",
+                              pop2ormore = "B02001_008"),
+                              state = "NY",
+                              year =  2017,
+                        output = "wide")
+
+write.csv(ny_2017, file = "D:\\Stadiums Project\\NY data\\raw\\ny_2017.csv")
+
